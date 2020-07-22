2b1e7d1d10efd179947b289ad306ff175f2eddf2
Apthornell <andrewthornellsound@gmail.com>
2020-07-21 22:51:39 -0500

    Corrected structure.

--- app.py
+++ app.py
@@ -19,18 +19,18 @@ inspector = inspect(engine)
 inspector.get_table_names()
 app = Flask(__name__)
 
-latestDate = (session.query(Measurement.date)
-                .order_by(Measurement.date.desc())
-                .first())
-latestDate = list(np.ravel(latestDate))[0]
+# latestDate = (session.query(Measurement.date)
+#                 .order_by(Measurement.date.desc())
+#                 .first())
+# latestDate = list(np.ravel(latestDate))[0]
 
-latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
-latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
-latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
-latestDay = int(dt.datetime.strftime(latestDate, '%d'))
+# latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
+# latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
+# latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
+# latestDay = int(dt.datetime.strftime(latestDate, '%d'))
 
-yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
-yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
+# yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
+# yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
 
 @app.route("/")
 def home():
@@ -40,7 +40,7 @@ def home():
            f"/api/v1.0/stations --- lists all weather observation stations<br/>"
            f"/api/v1.0/precipitation --- latest year of precipitation values<br/>"
            f"/api/v1.0/tobs --- latest year of temperature data<br/>"
-           f"/api/v1.0/temp/startdate ---gives temperature data from a given start date<br/>"
+           f"/api/v1.0/temp/startdate/ ---gives temperature data from a given start date<br/>"
            f"/api/v1.0/temp/startdate/enddate --- gives the temperature data for a given range of dates<br/>"
            f"date format is (YYYY-MM-DD)<br/>"
            f"dates available: 2010-01-01 to 2017-08-23")
@@ -54,22 +54,38 @@ def stations():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    results2 = session.query(Measurement.date, Measurement.prcp).filter_by(Measurement.date > yearBefore).all()
+    latestDate = (session.query(Measurement.date)
+                .order_by(Measurement.date.desc())
+                .first())
+    latestDate = list(np.ravel(latestDate))[0]
+
+    latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
+    latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
+    latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
+    latestDay = int(dt.datetime.strftime(latestDate, '%d'))
+    yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
+    yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
+
+    results2 = session.query(Measurement.date, Measurement.prcp).all()
     precipValues = {date: prcp for date, prcp in results2}
     return jsonify(precipValues)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    results3 = session.query(Measurement.tobs).filter_by(Station.name == "USC00519281").filter_by(Measurement.date > latestYear).all()
+    results3 = session.query(Measurement.tobs).filter(Station.station == "USC00519281").all()
     tobsData = list(np.ravel(results3))
     return jsonify(tobsData)
 
-@app.route("/api/v1.0/temp/<startdate>")
+@app.route("/api/v1.0/temp/<startdate>/")
 @app.route("/api/v1.0/temp/<startdate>/<enddate>")
 def tempDates(startdate=None, enddate=None):
-    if enddate != "":
+    latestDate = (session.query(Measurement.date)
+                 .order_by(Measurement.date.desc()).first())
+    latestDate = list(np.ravel(latestDate))[0]
+    latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
+    if enddate == "":
         tempStats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
-            .filter(Measurement.date.between(yearBefore, latestDate)).all()
+            .filter(Measurement.date.between(startdate, lastestDate)).all()
         tempData = list(np.ravel(tempStats))
         return jsonify(tempData)
     else:
